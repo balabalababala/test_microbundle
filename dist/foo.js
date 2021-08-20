616eaa3d0cf4a0e2f717dcc914261132815b019f
xueqin <shunan.ysn@antfin.com>
2021-08-20 20:35:28 +0800

    ci

--- dist/foo.js
+++ dist/foo.js
@@ -1,2 +1,8 @@
-function o(o){return o&&"object"==typeof o&&"default"in o?o:{default:o}}var e=o(require("lodash.get"));module.exports=function(){console.log(e.default({a:1},"a")),console.log("foo"),console.log("test")};
+function o(o) {
+  return o && 'object' == typeof o && 'default' in o ? o : { default: o };
+}
+var e = o(require('lodash.get'));
+module.exports = function () {
+  console.log(e.default({ a: 1 }, 'a')), console.log('foo'), console.log('test');
+};
 //# sourceMappingURL=foo.js.map
--- dist/foo.modern.js
+++ dist/foo.modern.js
@@ -1,2 +1,6 @@
-import o from"lodash.get";function l(){console.log(o({a:1},"a")),console.log("foo"),console.log("test")}export{l as default};
+import o from 'lodash.get';
+function l() {
+  console.log(o({ a: 1 }, 'a')), console.log('foo'), console.log('test');
+}
+export { l as default };
 //# sourceMappingURL=foo.modern.js.map
